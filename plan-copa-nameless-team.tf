70b55fcdf14e7155ebb2639fb80cae19025b2a4b
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-10-01 11:41:12 -0500

    ignore configs

---
+++ plan-copa-nameless-team.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create azure resource group
+resource "azurerm_resource_group" "rg_nameless" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = merge(var.tags)
+}
+
+# crear app service plan
+resource "azurerm_service_plan" "rg_nameless" {
+  name                = "nameless-az-service-plan"
+  resource_group_name = azurerm_resource_group.rg_nameless.name
+  location            = azurerm_resource_group.rg_nameless.location
+  sku_name            = var.sku_name
+  os_type             = var.os_type
+}
+
+
+# crea el web app con windows server
+resource "azurerm_windows_web_app" "rg_nameless" {
+  name                = "nameless-web-app"
+  resource_group_name = azurerm_resource_group.rg_nameless.name
+  location            = azurerm_service_plan.rg_nameless.location
+  service_plan_id     = azurerm_service_plan.rg_nameless.id
+
+  site_config {
+
+  }
+}
