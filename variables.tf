d35ebb22ef44b86dfe61728f1173668d8b1f13c8
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-10-01 11:33:43 -0500

    service app, tags

--- variables.tf
+++ variables.tf
@@ -28,3 +28,48 @@ variable "address_space" {
   description = "The address space that is used by the virtual network."
   default     = ["172.16.0.0/24"]
 }
+
+# prefijo de la subnet
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = ["172.16.0.0/24"]
+}
+
+
+# imagen del sistema operativo
+variable "image_offer" {
+  description = "Name of the offer (az vm image list)"
+  default     = "UbuntuServer"
+}
+
+# sku 
+variable "image_sku" {
+  description = "Image SKU to apply "
+  default     = "18.04-LTS"
+}
+
+variable "image_version" {
+  description = "Version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+
+variable "sku_name" {
+  descripton = "nombre del sku"
+  default = "P1v2"
+}
+
+variable "os_type" {
+  description = "tipo de sistema operativo"
+  default = "Windows"
+}
+
+variable "tags" {
+    description = "tags de los recursos"
+    default = {
+        "CREATEDBY": "NAMELESS",
+        "creadorDPT": "VENTAS",
+        "AMBIENTE": "PRD"
+    }  
+}
+
+
